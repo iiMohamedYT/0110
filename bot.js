3329bc8adaff23c3fd053abf7f22b9f0f2bcf4e2
iiMohamedYT <45318130+iiMohamedYT@users.noreply.github.com>
2019-07-24 20:51:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ client.on('message', message => {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'sets')) {
+  if (message.content.startsWith(adminprefix + '')) {
     client.user.setGame(argresult, "https://www.twitch.tv/dream");
       message.channel.send(`**✅**`)
   }
